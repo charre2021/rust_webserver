ec3cb49de746a43b595a18ff5553a8cdf9ffa899
Chris Harrelson <charre2021@outlook.com>
2026-02-25 12:37:18 -0600

    2/3 way through

---
+++ rust_webserver/src/lib.rs
@@ -0,0 +1,97 @@
+use std::{sync::{Arc, Mutex, mpsc}, thread};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver
+                    .lock()
+                    .expect("Mutex is in a poisoned state")
+                    .recv()
+                    .unwrap();
+
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
+        });
+
+        Worker { id, thread }
+    }
+}
+
+#[derive(Debug)]
+pub enum PoolCreationErrorType {
+    OverSubscribed(String),
+    UnderSubscribed(String),
+}
+
+#[derive(Debug)]
+pub struct PoolCreationError {
+    pub error_type: PoolCreationErrorType,
+}
+
+
+impl PoolCreationError {
+    fn new(error_type: PoolCreationErrorType) -> Self {
+        PoolCreationError { error_type }
+    }
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    /// 
+    /// The size is the number of threads in the pool.
+    /// 
+    /// # Panics
+    /// 
+    /// The `new` function will panic if the size is zero.
+    pub fn build(size: i32) -> Result<ThreadPool, PoolCreationError> {
+        if size < 0 {
+           Err(PoolCreationError::new(
+            PoolCreationErrorType::UnderSubscribed(
+                String::from("Too few threads in the pool.")
+            )
+        ))
+        } else if size > 4 {
+            Err(PoolCreationError::new(
+            PoolCreationErrorType::OverSubscribed(
+                String::from("Too many threads in the pool.")
+            )
+        ))
+        } else {
+            let unsigned_size = size as usize;
+            let (sender, receiver) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+            let mut workers = Vec::with_capacity(unsigned_size);
+            for id in 0..unsigned_size {
+                workers.push(Worker::new(id, Arc::clone(&receiver)));
+
+            }
+
+            Ok(ThreadPool { workers, sender })
+        }
+
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+        {
+            let job = Box::new(f);
+
+            self.sender.send(job).unwrap();
+        }
+}
---
+++ rust_webserver/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+use rust_webserver::ThreadPool;
+
+
+fn main() {
+    let port = "7878";
+    let pool_size: i32 = 4;
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    let pool = match ThreadPool::build(pool_size) {
+        Ok(p) => p,
+        Err(e) => panic!("Returned error on thread pool creation: {:?}", e.error_type),
+    };
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    
+    stream.write_all(response.as_bytes()).unwrap();
+    }
